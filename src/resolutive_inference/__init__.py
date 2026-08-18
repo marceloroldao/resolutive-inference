17b7f0cf1f8f06f0ef5fcc615f7b4095897ed2ed
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-18 10:31:34 -0300

    feat: export CompactRobust119 reference

--- src/resolutive_inference/__init__.py
+++ src/resolutive_inference/__init__.py
@@ -1,7 +1,8 @@
 """Compact components for experimental sequential inference."""
 
 from .compact import CompactPro
+from .compact_robust import CompactRobust119
 from .fixed_point import FixedPointViterbi
 
-__all__ = ["CompactPro", "FixedPointViterbi"]
+__all__ = ["CompactPro", "CompactRobust119", "FixedPointViterbi"]
 __version__ = "0.1.0"

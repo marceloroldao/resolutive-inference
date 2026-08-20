80f1bdf5b241053c0942b13acdbcf9e9e1fa8558
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-20 02:26:45 -0300

    api: expose integer edge runtime references

--- src/resolutive_inference/__init__.py
+++ src/resolutive_inference/__init__.py
@@ -3,6 +3,13 @@
 from .compact import CompactPro
 from .compact_robust import CompactRobust119
 from .fixed_point import FixedPointViterbi
+from .integer_runtime import IntegerEmissionRuntime, IntegerLag8Decoder
 
-__all__ = ["CompactPro", "CompactRobust119", "FixedPointViterbi"]
+__all__ = [
+    "CompactPro",
+    "CompactRobust119",
+    "FixedPointViterbi",
+    "IntegerEmissionRuntime",
+    "IntegerLag8Decoder",
+]
 __version__ = "0.1.0"
